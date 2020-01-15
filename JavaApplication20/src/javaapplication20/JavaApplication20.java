be205e81dbd2aa157f3d906d6a619ee74a167c55
Andy Riedlinger <technicalandyr@gmail.com>
2020-01-15 09:40:16 -0500

    Changed Print

--- JavaApplication20/src/javaapplication20/JavaApplication20.java
+++ JavaApplication20/src/javaapplication20/JavaApplication20.java
@@ -16,7 +16,7 @@ public class JavaApplication20 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("HELLO WORLD!");
+        System.out.println("I changed this print");
     }
     
 }
