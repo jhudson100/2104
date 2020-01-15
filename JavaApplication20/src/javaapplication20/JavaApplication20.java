34c2f4ef7aaa8c7146eb8573b2828d5800e084ae
Andy Riedlinger <technicalandyr@gmail.com>
2020-01-15 09:46:36 -0500

    Deleted print

--- JavaApplication20/src/javaapplication20/JavaApplication20.java
+++ JavaApplication20/src/javaapplication20/JavaApplication20.java
@@ -16,7 +16,6 @@ public class JavaApplication20 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("I changed this print");
     }
     
 }
